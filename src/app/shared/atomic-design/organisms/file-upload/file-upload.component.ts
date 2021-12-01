583e307a57e2be652ab6784e0db743c9c7038bcb
Rafael Luiz Rehn <rafaelluizrehn@gmail.com>
2021-11-30 23:47:26 -0300

    Add file upload

--- src/app/shared/atomic-design/organisms/file-upload/file-upload.component.ts
+++ src/app/shared/atomic-design/organisms/file-upload/file-upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { FileUpload } from './model/file-upload.model';
+import { FileUploadService } from './service/file-upload.service';
 
 @Component({
   selector: 'app-file-upload',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
+  selectedFiles?: FileList;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
+
+  fileUploads?: any[];
+
+  constructor(private uploadService: FileUploadService) { }
 
   ngOnInit(): void {
+    this.getListFiles()
+  } 
+
+  getListFiles(){
+    this.uploadService.getFiles(6).snapshotChanges().pipe(
+      map(changes =>
+        // store the key
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    ).subscribe(fileUploads => {
+      this.fileUploads = fileUploads;
+    });
   }
 
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload(): void {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+
+      if (file) {
+        this.currentFileUpload = new FileUpload(file);
+        this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(
+          percentage => {
+            this.percentage = Math.round(percentage ? percentage : 0);
+
+              // aqui escrever a regra a key dos arquivos
+
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+    }
+
+  }
 }
