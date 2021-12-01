93a96c1d3f3f3a0533977f802c4f04c798935d6a
Rafael Luiz Rehn <rafaelluizrehn@gmail.com>
2021-11-30 23:20:56 -0300

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -73,8 +73,8 @@ const ROUTES: Routes = [
 
     ]
   },
-  // { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  // { path: '**', redirectTo: '/auth/login' },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/auth/login' },
 ];
 
 @NgModule({
