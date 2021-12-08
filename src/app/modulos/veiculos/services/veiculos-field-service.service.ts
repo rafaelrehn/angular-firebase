a9fd441ef45fb4df954686538b463870a168782a
Rafael Rehn <rafael.rehn@projuris.com.br>
2021-12-08 14:01:47 -0300

    ajustes e correcoes gerais

--- src/app/modulos/veiculos/services/veiculos-field-service.service.ts
+++ src/app/modulos/veiculos/services/veiculos-field-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IInputInterface, IInputType, ISelectOptions } from 'src/app/shared/atomic-design/atoms/input/input.interface';
+import { IInputInterface, IInputType } from 'src/app/shared/atomic-design/atoms/input/input.interface';
 import { AbstractFieldsService } from 'src/app/shared/base/abstract-fields.interface';
 import { IFieldBuilder } from 'src/app/shared/base/field-buider';
 import { CAMBIO_OPTIONS, COMBUSTIVEL_OPTIONS, COR_OPTIONS, ITENS_ADICIONAIS_OPTIONS } from './veiculos-fields.options';
@@ -16,7 +16,7 @@ export class VeiculosFieldServiceService implements AbstractFieldsService {
 
   buildFields(): IInputInterface[]{
     return [
-      new IFieldBuilder().build('nome').label('Nome').columnShow().get(),
+      new IFieldBuilder().build('nome').label('Nome').columnShow().required().get(),
       new IFieldBuilder().build('marca').label('Marca').columnShow().get(),
       new IFieldBuilder().build('anoFabricacao').label('Ano de fabricação').setClass('w-140').get(),
       new IFieldBuilder().build('anoModelo').label('Ano Modelo').setClass('w-140').get(),
